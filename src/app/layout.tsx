1f6b40646cb5e77e16b7109ebc866493122dfcd9
Afnan-byte <afnan.messaging@gmail.com>
2026-05-06 21:35:33 +0530

    Fix hydration error caused by browser extensions injecting attributes into body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${outfit.variable} ${playfair.variable} font-sans antialiased`}
+        suppressHydrationWarning
       >
         <AuthProvider>
           {children}
